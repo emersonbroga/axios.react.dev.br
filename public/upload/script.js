e4dcf331e544d83dd65cf2a67b9886c98208f7ce
Emerson Broga <emerson@emersonbroga.com>
2022-03-13 20:07:22 -0300

    Added upload script

---
+++ public/upload/script.js
@@ -0,0 +1,36 @@
+const formFind = document.querySelector('[data-form-find]');
+const formUser = document.querySelector('[data-form-user]');
+const uploadButton = document.querySelector('[data-upload]');
+const avatarInput = document.querySelector('[data-avatar]');
+
+const handleFindSumbit = (e) => {
+  e.preventDefault();
+  const data = getFormData(formFind);
+  const id = data.get('id');
+  const url = apiUser(id);
+
+  axios.get(url).then(printResponse).then(populate);
+};
+
+const handleUserSumbit = (e) => {
+  e.preventDefault();
+  const data = getFormData(formUser);
+  const id = data.get('id');
+  const url = apiUser(id, 'upload');
+
+  const options = {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  };
+
+  axios.patch(url, data, options).then(printResponse);
+};
+
+const handleUploadClick = (e) => {
+  avatarInput.value = e.target.files[0].name;
+};
+
+formFind.addEventListener('submit', handleFindSumbit);
+formUser.addEventListener('submit', handleUserSumbit);
+uploadButton.addEventListener('change', handleUploadClick);
