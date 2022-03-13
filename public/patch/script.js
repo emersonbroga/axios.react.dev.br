1680c47600886309696424b5cef41cd766a85bdb
Emerson Broga <emerson@emersonbroga.com>
2022-03-13 19:32:58 -0300

    Added patch script

---
+++ public/patch/script.js
@@ -0,0 +1,24 @@
+const formFind = document.querySelector('[data-form-find]');
+const formUser = document.querySelector('[data-form-user]');
+
+const handleFindSubmit = (e) => {
+  e.preventDefault();
+  const data = getFormData(formFind);
+  const id = data.get('id');
+  const url = apiUser(id);
+
+  axios.get(url).then(printResponse).then(populate);
+};
+
+const handleUserSubmit = (e) => {
+  e.preventDefault();
+
+  const data = getFormData(formUser);
+  const id = data.get('id');
+  const url = apiUser(id);
+
+  axios.patch(url, data).then(printResponse);
+};
+
+formFind.addEventListener('submit', handleFindSubmit);
+formUser.addEventListener('submit', handleUserSubmit);
