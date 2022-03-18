43fe2b058b3a1f85c551859c175156c531d9a3fb
Emerson Broga <emerson@emersonbroga.com>
2022-03-17 21:14:14 -0300

    Added upload-progress-bar-script

---
+++ public/upload-progress-bar/script.js
@@ -0,0 +1,47 @@
+const formFind = document.querySelector('[data-form-find]');
+const formUser = document.querySelector('[data-form-user]');
+const avatarInput = document.querySelector('[data-avatar]');
+const uploadButton = document.querySelector('[data-upload]');
+const progressBar = document.querySelector('[data-progress]');
+
+const handleFindSubmit = (e) => {
+  e.preventDefault();
+  const data = getFormData(formFind);
+  const id = data.get('id');
+  const url = apiUser(id);
+
+  axios.get(url).then(printResponse).then(populate);
+};
+
+const handleUserSubmit = (e) => {
+  e.preventDefault();
+  const data = getFormData(formUser);
+  const id = data.get('id');
+  const url = apiUser(id, 'upload');
+
+  const options = {
+    url,
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    onUploadProgress: function (progressEvent) {
+      const percentage = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+      progressBar.style.width = `${percentage}%`;
+      progressBar.innerText = `${percentage}%`;
+      progressBar.parentNode.style.display = percentage === 100 ? 'none' : 'block';
+      console.log(percentage);
+    },
+  };
+
+  axios(options).then(printResponse);
+  // axios.patch(url, data, options).then(printResponse);
+};
+
+const handleUploadChange = (e) => {
+  avatarInput.value = e.target.files[0].name;
+};
+
+formFind.addEventListener('submit', handleFindSubmit);
+formUser.addEventListener('submit', handleUserSubmit);
+uploadButton.addEventListener('change', handleUploadChange);
