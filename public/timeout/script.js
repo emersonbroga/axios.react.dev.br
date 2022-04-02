3e980bbf98da659a76afe10d457af89bf5fb8a82
Emerson Broga <emerson@emersonbroga.com>
2022-04-02 10:57:17 -0300

    Added timeout script

---
+++ public/timeout/script.js
@@ -0,0 +1,17 @@
+const formFind = document.querySelector('[data-form-find]');
+
+const handleFindSubmit = (e) => {
+  e.preventDefault();
+
+  const data = getFormData(formFind);
+  const id = data.get('id');
+  const url = apiUser(id, 'slow');
+
+  const options = {
+    timeout: 3000,
+  };
+
+  axios.get(url, options).then(printResponse).catch(console.log);
+};
+
+formFind.addEventListener('submit', handleFindSubmit);
